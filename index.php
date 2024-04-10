a167b9dfb51d2fc28841aee62f5d38be313ed5d1
Pavel Osipov <154364350+yaosipovpavel@users.noreply.github.com>
2024-04-11 01:19:41 +0500

    Add four stroke and align code

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP 11.7</title>
 </head>
+
 <body>
     <table border="1">
         <thead>
@@ -19,162 +21,238 @@
                 </tr>
             </thead>
 
-            <tbody>
-                <!--Первая строка -->
-                <tr>
-                    <th>0</th>
-                    <th>0</th>
-                    <th>
-                        <?php
-                        $a=0;
-                        if($a === 0){
-                            echo 1;
-                        }else{
-                            echo $a;
-                        }
-                        ?>
-                    </th>
-                    <th>
+        <tbody>
+            <!--Первая строка -->
+            <tr>
+                <th><?php
+                    $a = 0;
+                    echo $a;
+                    ?></th>
+                <th><?php
+                    $b = 0;
+                    echo $b;
+                    ?></th>
+                <th>
                     <?php
-                        $a=0;
-                        $b=0;
-                        $c=($a || $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-                    </th>
-                    <th>
+                    $a = 0;
+                    if ($a === 0) {
+                        echo 1;
+                    } else {
+                        echo 0;
+                    }
+                    ?>
+                </th>
+                <th>
                     <?php
-                        $a=0;
-                        $b=0;
-                        $c=($a && $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-                    </th>
-                    <th>
+                    $a = 0;
+                    $b = 0;
+                    $c = ($a || $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+                <th>
                     <?php
-                        $a=0;
-                        $b=0;
-                        $c=($a xor $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-                    </th>
-                </tr>
-<!--Вторая строка -->
-<tr>
-    <th>0</th>
-    <th>1</th>
-    <th>
-    <?php
-                        $a=0;
-                        if($a === 0){
-                            echo 1;
-                        }else{
-                            echo $a;
-                        }
-                        ?>
-    </th>
-    <th>
-    <?php
-                        $a=0;
-                        $b=1;
-                        $c=($a || $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-    </th>
-    <th>
-    <?php
-                        $a=0;
-                        $b=1;
-                        $c=($a && $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-    </th>
-    <th>
-<?php
-                        $a=0;
-                        $b=1;
-                        $c=($a xor $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-</th>
-</tr>
-<!--Третья строка -->
-<tr>
-    <th>1</th>
-    <th>0</th>
-    <th>
-    <?php
-                        $a=1;
-                        if($a === 1){
-                            echo 0;
-                        }else{
-                            echo $a;
-                        }
-                        ?>
-    </th>
-    <th>
-    <?php
-                        $a=1;
-                        $b=0;
-                        $c=($a || $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-    </th>
-    <th>
-    <?php
-                        $a=1;
-                        $b=0;
-                        $c=($a && $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-    </th>
-    <th>
-    <?php
-                        $a=1;
-                        $b=0;
-                        $c=($a xor $b);
-                        if($c === false){
-                            echo 0;
-                        }else{
-                            echo $c;
-                        }
-                        ?>
-    </th>
-</tr>
-            </tbody>
+                    $a = 0;
+                    $b = 0;
+                    $c = ($a && $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 0;
+                    $b = 0;
+                    $c = ($a xor $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+            </tr>
+            <!--Вторая строка -->
+            <tr>
+                <th><?php
+                    $a = 0;
+                    echo $a;
+                    ?></th>
+                <th><?php
+                    $b = 1;
+                    echo $b;
+                    ?></th>
+                <th>
+                    <?php
+                    $a = 0;
+                    if ($a === 0) {
+                        echo 1;
+                    } else {
+                        echo 0;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 0;
+                    $b = 1;
+                    $c = ($a || $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 0;
+                    $b = 1;
+                    $c = ($a && $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 0;
+                    $b = 1;
+                    $c = ($a xor $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+            </tr>
+            <!--Третья строка -->
+            <tr>
+                <th><?php
+                    $a = 1;
+                    echo $a;
+                    ?></th>
+                <th><?php
+                    $b = 0;
+                    echo $b;
+                    ?></th>
+                <th>
+                    <?php
+                    $a = 1;
+                    if ($a === 1) {
+                        echo 0;
+                    } else {
+                        echo 1;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 1;
+                    $b = 0;
+                    $c = ($a || $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 1;
+                    $b = 0;
+                    $c = ($a && $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 1;
+                    $b = 0;
+                    $c = ($a xor $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+            </tr>
+            <!--Четвертая строка -->
+            <tr>
+                <th><?php
+                    $a = 1;
+                    echo $a;
+                    ?></th>
+                <th><?php
+                    $b = 1;
+                    echo $b;
+                    ?></th>
+                <th>
+                    <?php
+                    $a = 1;
+                    if ($a === 1) {
+                        echo 0;
+                    } else {
+                        echo 1;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 1;
+                    $b = 1;
+                    $c = ($a || $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 1;
+                    $b = 1;
+                    $c = ($a && $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+                <th>
+                    <?php
+                    $a = 1;
+                    $b = 1;
+                    $c = ($a xor $b);
+                    if ($c === false) {
+                        echo 0;
+                    } else {
+                        echo $c;
+                    }
+                    ?>
+                </th>
+            </tr>
+        </tbody>
         </thead>
     </table>
 </body>
+
 </html>
