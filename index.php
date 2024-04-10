a2a48b2ccdab09e10ae7aa9feb546feee69c016f
Pavel Osipov <154364350+yaosipovpavel@users.noreply.github.com>
2024-04-11 01:07:14 +0500

    Add 2 and 3 stroke

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
             </thead>
 
             <tbody>
+                <!--Первая строка -->
                 <tr>
                     <th>0</th>
                     <th>0</th>
@@ -70,6 +71,108 @@
                         ?>
                     </th>
                 </tr>
+<!--Вторая строка -->
+<tr>
+    <th>0</th>
+    <th>1</th>
+    <th>
+    <?php
+                        $a=0;
+                        if($a === 0){
+                            echo 1;
+                        }else{
+                            echo $a;
+                        }
+                        ?>
+    </th>
+    <th>
+    <?php
+                        $a=0;
+                        $b=1;
+                        $c=($a || $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+    </th>
+    <th>
+    <?php
+                        $a=0;
+                        $b=1;
+                        $c=($a && $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+    </th>
+    <th>
+<?php
+                        $a=0;
+                        $b=1;
+                        $c=($a xor $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+</th>
+</tr>
+<!--Третья строка -->
+<tr>
+    <th>1</th>
+    <th>0</th>
+    <th>
+    <?php
+                        $a=1;
+                        if($a === 1){
+                            echo 0;
+                        }else{
+                            echo $a;
+                        }
+                        ?>
+    </th>
+    <th>
+    <?php
+                        $a=1;
+                        $b=0;
+                        $c=($a || $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+    </th>
+    <th>
+    <?php
+                        $a=1;
+                        $b=0;
+                        $c=($a && $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+    </th>
+    <th>
+    <?php
+                        $a=1;
+                        $b=0;
+                        $c=($a xor $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+    </th>
+</tr>
             </tbody>
         </thead>
     </table>
