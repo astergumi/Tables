8b3cdaabe507db0f4e979791ef3c000cd0c1cfee
Pavel Osipov <154364350+yaosipovpavel@users.noreply.github.com>
2024-04-11 00:54:01 +0500

    add first stroke

--- index.php
+++ index.php
@@ -6,6 +6,72 @@
     <title>PHP 11.7</title>
 </head>
 <body>
-    
+    <table border="1">
+        <thead>
+            <thead>
+                <tr>
+                    <th>A</th>
+                    <th>B</th>
+                    <th>!A</th>
+                    <th>A || B</th>
+                    <th>A && B</th>
+                    <th>A xor B</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th>0</th>
+                    <th>0</th>
+                    <th>
+                        <?php
+                        $a=0;
+                        if($a === 0){
+                            echo 1;
+                        }else{
+                            echo $a;
+                        }
+                        ?>
+                    </th>
+                    <th>
+                    <?php
+                        $a=0;
+                        $b=0;
+                        $c=($a || $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+                    </th>
+                    <th>
+                    <?php
+                        $a=0;
+                        $b=0;
+                        $c=($a && $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+                    </th>
+                    <th>
+                    <?php
+                        $a=0;
+                        $b=0;
+                        $c=($a xor $b);
+                        if($c === false){
+                            echo 0;
+                        }else{
+                            echo $c;
+                        }
+                        ?>
+                    </th>
+                </tr>
+            </tbody>
+        </thead>
+    </table>
 </body>
 </html>
